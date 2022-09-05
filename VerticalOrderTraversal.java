3b57aaa4605a3e42efee6ef6f2e90ac525d5b747
Mehul Porwal <64722605+mehulgithub-1903@users.noreply.github.com>
2022-09-05 14:17:46 +0530

    Update VerticalOrderTraversal.java

--- VerticalOrderTraversal.java
+++ VerticalOrderTraversal.java
@@ -1,68 +1,66 @@
-/************************************************************
-
-    Following is the TreeNode class structure
-
-    class TreeNode<T> 
-    {
-       public:
-        T data;
-        TreeNode<T> left;
-        TreeNode<T> right;
-
-        TreeNode(T data) 
-        {
-            this.data = data;
-            left = null;
-            right = null;
-        }
-    };
-
-************************************************************/
-
-import java.util.*;
-
-public class Solution 
-{
-    static class Pair{
-        int val;
-        int level;
-
-        Pair(int val,int level){
-            this.val=val;
-            this.level=level;
-        }
-    }
-    public static ArrayList<Integer> verticalOrderTraversal(TreeNode<Integer> root) 
-    {
-        if(root==null) return new ArrayList<Integer>();
-        TreeMap<Integer,ArrayList<Pair>> mp=new TreeMap<>();
-        ArrayList<Integer> al=new ArrayList<Integer>();
-         
-        dfs(root,0,mp,0);
-         
-        for(Map.Entry<Integer,ArrayList<Pair>> e:mp.entrySet()){
-            ArrayList<Pair> temp=e.getValue();
-            Collections.sort(temp,(Pair a,Pair b)->a.level-b.level);
-            for(int i=0;i<temp.size();i++){
-                al.add(temp.get(i).val);
-            }
-        }
-         
-        return al;
-    }
-     public static void dfs(TreeNode<Integer> root,int vl,TreeMap<Integer,ArrayList<Pair>> mp,int level){
-        if(root==null) return;
-        if(!mp.containsKey(vl)){
-            mp.put(vl,new ArrayList<Pair>());
-            mp.get(vl).add(new Pair(root.data,level));
-        }
-         else{
-             mp.get(vl).add(new Pair(root.data, level));
-         }
-
-        dfs(root.left, vl-1, mp,level+1);
-        dfs(root.right, vl+1, mp,level+1);
-
-        return;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Tuple {
+    TreeNode node;
+    int row;
+    int col;
+    public Tuple(TreeNode _node, int _row, int _col) {
+        node = _node;
+        row = _row;
+        col = _col;
     }
 }
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap < Integer, TreeMap < Integer, PriorityQueue < Integer >>> map = new TreeMap < > ();
+        Queue < Tuple > q = new LinkedList < Tuple > ();
+        q.offer(new Tuple(root, 0, 0));
+        while (!q.isEmpty()) {
+            Tuple tuple = q.poll();
+            TreeNode node = tuple.node;
+            int x = tuple.row;
+            int y = tuple.col;
+
+
+            if (!map.containsKey(x)) {
+                map.put(x, new TreeMap < > ());
+            }
+            if (!map.get(x).containsKey(y)) {
+                map.get(x).put(y, new PriorityQueue < > ());
+            }
+            map.get(x).get(y).offer(node.val);
+
+            if (node.left != null) {
+                q.offer(new Tuple(node.left, x - 1, y + 1));
+            }
+            if (node.right != null) {
+                q.offer(new Tuple(node.right, x + 1, y + 1));
+            }
+        }
+        List < List < Integer >> list = new ArrayList < > ();
+        for (TreeMap < Integer, PriorityQueue < Integer >> ys: map.values()) {
+            list.add(new ArrayList < > ());
+            for (PriorityQueue < Integer > nodes: ys.values()) {
+                while (!nodes.isEmpty()) {
+                    list.get(list.size() - 1).add(nodes.poll());
+                }
+            }
+        }
+        return list;
+    }
+}
+// tc= o(nlognlognlogn)
+// sc=o(n)
